f8f29b97a03a04b09589ee7fd9e039b45af72e61
Sarvar Omonov <sarvaromon33@gmail.com>
2025-03-31 01:03:25 +0500

    Changed from Webhook to Polling

--- kino_bot/bot.py
+++ kino_bot/bot.py
@@ -2,8 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiohttp import web
-import os
+
 
 from config import BOT_TOKEN
 from database import create_db
@@ -17,15 +16,6 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
 
-async def handle_request(request):
-    try:
-        update = await request.json()
-        await dp.feed_webhook_update(bot, update)
-        return web.Response(text="OK")
-    except Exception as e:
-        logging.error(f"Xatolik: {e}")
-        return web.Response(status=500, text="Xatolik yuz berdi")
-
 def setup_middlewares():
     dp.update.middleware(CheckSubscriptionMiddleware())
 
@@ -36,30 +26,13 @@ async def on_startup():
     register_handlers(dp)
     register_start_handler(dp)
 
-    # Webhook URL
-    WEBHOOK_HOST = os.environ.get("RENDER_EXTERNAL_HOSTNAME", "https://movie-bot-i2lc.onrender.com")
-    WEBHOOK_URL = f"https://{WEBHOOK_HOST}/webhook"
-
-    logging.info(f"Webhook o‘rnatilmoqda: {WEBHOOK_URL}")
-    await bot.set_webhook(WEBHOOK_URL)
-
-async def on_shutdown():
-    logging.info("Webhook o‘chirilmoqda...")
-    await bot.delete_webhook()
-
 async def main():
     await on_startup()
-
-    # Web server yaratish
-    app = web.Application()
-    app.router.add_post("/webhook", handle_request)
-
-    # Port sozlash
-    PORT = int(os.environ.get("PORT", 8080))
-
-    logging.info(f"Server {PORT}-portda ishlamoqda...")
-    return app, PORT
+    logging.info("Polling boshlandi...")
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    app, port = asyncio.run(main())
-    web.run_app(app, host="0.0.0.0", port=port)
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot to‘xtatildi.")
