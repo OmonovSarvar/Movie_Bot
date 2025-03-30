9906b45d972fd68d655fd19f61cb808727429115
Sarvar Omonov <sarvaromon33@gmail.com>
2025-03-30 22:43:44 +0500

    Changed from Polling to Webhook in bot.py

--- kino_bot/bot.py
+++ kino_bot/bot.py
@@ -6,6 +6,7 @@ from config import BOT_TOKEN
 from database import create_db
 from handlers import register_handlers, register_start_handler
 from middlewares import CheckSubscriptionMiddleware
+from aiohttp import web
 import os
 
 # Logging sozlamalari
@@ -15,19 +16,36 @@ logging.basicConfig(level=logging.INFO)
 dp = Dispatcher(storage=MemoryStorage())
 bot = Bot(token=BOT_TOKEN)
 
+async def handle_request(request):
+    """Telegram webhook ma'lumotlarini qabul qilish"""
+    update = await request.json()
+    await dp.feed_webhook_update(bot, update)
+    return web.Response()
+
 def setup_middlewares():
     """Middlewarelarni o'rnatish"""
     dp.update.middleware(CheckSubscriptionMiddleware())
 
 async def main():
-    """Asosiy ishga tushirish funksiyasi"""
     logging.info("Bot ishga tushmoqda...")
-    os.environ.get("PORT", 8080)
-    create_db()  # Ma'lumotlar bazasini yaratish
+    create_db()
     setup_middlewares()
-    register_handlers(dp)# Handlerlarni ro‘yxatga olish
+    register_handlers(dp)
     register_start_handler(dp)
-    await dp.start_polling(bot)
+
+    # Webhook URL
+    WEBHOOK_URL = f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/webhook"
+
+    # Webhook o‘rnatish
+    await bot.set_webhook(WEBHOOK_URL)
+
+    # Web serverni boshlash
+    app = web.Application()
+    app.router.add_post("/webhook", handle_request)
+
+    PORT = int(os.environ.get("PORT", 8080))
+    return app, PORT
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app, port = asyncio.run(main())
+    web.run_app(app, host="0.0.0.0", port=port)
